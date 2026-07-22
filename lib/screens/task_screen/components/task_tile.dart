62927f0f8d793326d089eca889684883b10c50ee
Raghuvansh Agarwal <rarchva@gmail.com>
2026-07-22 08:49:35 +0530

    ui logic: tasks can be checked off and unchecked

--- lib/screens/task_screen/components/task_tile.dart
+++ lib/screens/task_screen/components/task_tile.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({super.key, required this.task});
 
   final String task;
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool _isChecked = false;
+  @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.all(0),
-      title: Text(task),
-      trailing: Checkbox(value: true, onChanged: (newValue) {}),
+      title: Text(
+        widget.task,
+        style: TextStyle(
+          decoration: _isChecked ? TextDecoration.lineThrough : null,
+          color: Theme.of(
+            context,
+          ).colorScheme.onPrimaryContainer.withAlpha(_isChecked ? 150 : 255),
+        ),
+      ),
+      trailing: Checkbox(
+        value: _isChecked,
+        onChanged: (newValue) {
+          setState(() {
+            _isChecked = newValue!;
+          });
+        },
+      ),
     );
   }
 }
